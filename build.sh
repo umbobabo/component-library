26b7432faeda6594f13b9fa420326c037856e9ef
Fábio Santos <fabiosantosart@gmail.com>
2015-10-20 12:03:56 +0100

    use SAUCE_USER variable

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ SSH_KEY=${1:-/root/.ssh/id_rsa}
 
 [[ ${NPM_TOKEN:-} = '' ]] && { echo "NPM_TOKEN empty"; exit 1; }
 [[ ${SAUCE_ACCESS_KEY:-} = '' ]] && { echo "SAUCE_ACCESS_KEY empty"; exit 2; }
+[[ ${SAUCE_USER:-} = '' ]] && { echo "SAUCE_USER empty"; exit 3; }
 
 docker pull sublimino/node4-base
 
@@ -17,7 +18,8 @@ exec docker run \
         umask 000 && \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > ~/.npmrc && \
         npm i && \
-        echo SAUCE_USER=sublimino SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
+        echo npm run doc:js
+        echo SAUCE_USER=${SAUCE_USER} SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
         { git config --global user.email 'ecprod@economist.com'; git config --global user.name 'GoCD'; true; } && \
         { [ \"$(git rev-parse --abbrev-ref HEAD)\" != \"master\" ] || npm run pages; } ; \
         RETURN_CODE=\$?; \
