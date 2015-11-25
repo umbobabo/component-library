6ec37ecc3f7b727e7a6d5a420d156f5a8eb60a60
Andrew Martin <sublimino@gmail.com>
2015-11-25 16:57:58 +0000

    fix(tests): enable build.sh tests
    Also updates karma.conf.js

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SSH_KEY=${1:-/root/.ssh/id_rsa}
+SSH_KEY="${1:-/root/.ssh/id_rsa}"
+DOCKER_IMAGE="economistprod/node4-base"
 HOST_IP=$(ip route get 1 | awk '{print $NF;exit}')
 SINOPIA_URL="http://${HOST_IP}:4873"
 
@@ -8,12 +9,13 @@ SINOPIA_URL="http://${HOST_IP}:4873"
 [[ ${SAUCE_ACCESS_KEY:-} = '' ]] && { echo "SAUCE_ACCESS_KEY empty"; exit 2; }
 [[ ${SAUCE_USERNAME:-} = '' ]] && { echo "SAUCE_USERNAME empty"; exit 3; }
 
-docker pull sublimino/node4-base
+docker pull "${DOCKER_IMAGE}"
 
 exec docker run \
     -v "${SSH_KEY}":/root/.ssh/id_rsa \
-    -v $(pwd):/code \
-    sublimino/node4-base \
+    -v "$(pwd)":/code \
+    -e NODE_ENV=${NODE_ENV:-test} \
+    "${DOCKER_IMAGE}" \
     /bin/sh -cx "\
         trap 'chmod 777 node_modules -R' EXIT && \
         cd /code && \
@@ -21,8 +23,8 @@ exec docker run \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > ~/.npmrc && \
         (curl -I ${SINOPIA_URL} --max-time 5 && npm set registry ${SINOPIA_URL} && echo \"Using sinopia cache registry available on ${SINOPIA_URL}\" || true) && \
         npm i && \
-        npm run doc:js
-        echo SAUCE_USERNAME=${SAUCE_USERNAME} SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
+        npm run doc:js && \
+        SAUCE_USERNAME=${SAUCE_USERNAME} SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
         { git config --global user.email 'ecprod@economist.com'; git config --global user.name 'GoCD'; true; } && \
         { [ \"$(git rev-parse --abbrev-ref HEAD)\" != \"master\" ] || npm run pages; } ; \
         RETURN_CODE=\$?; \
